9432fe09c3516141d4be27ffe41ade3732384240
japankid-code <japankid.jake@gmail.com>
2021-04-24 11:19:53 -0500

    add user prompts for each section, close #1

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,138 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your application?',
+            validate: titleInput => {
+                if  (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title !!');
+                    return false;
+                }
+            },
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Briefly describe the app:',
+            validate: descriptionInput => {
+                if  (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Please describe the app!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'What commands are needed to get the app working?',
+            validate: installationInput => {
+                if  (installationInput) {
+                    return true;
+                } else {
+                    console.log('Please enter commands used!!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'More detailed info about how to use:',
+            validate: usageInput => {
+                if  (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your input !!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmScreenies',
+            message: 'Would you like to add a link to a screenshot?',
+            default: false,
+        },
+            {
+                type: 'input',
+                name: 'screenshots',
+                message: 'URLs, separated by commas:',
+                when: ({ confirmScreenies }) => {
+                    if (confirmScreenies) {
+                    return true;
+                    } else {
+                    return false;
+                    }
+                },
+            },
+        {
+            type: 'confirm',
+            name: 'confirmContribs',
+            message: 'Are there any sources that need attributing?',
+            default: false,
+        },
+            {
+                type: 'input',
+                name: 'contributions',
+                message: 'Links, separated by commas:',
+                when: ({ confirmContribs }) => {
+                    if (confirmContribs) {
+                    return true;
+                    } else {
+                    return false;
+                    }
+                },
+            },
+        {
+            type: 'confirm',
+            name: 'confirmTests',
+            message: 'Were any tests made for the app??',
+            default: false,
+        },
+            {
+                type: 'input',
+                name: 'tests',
+                message: 'Describe testing process:',
+                when: ({ confirmTests }) => {
+                    if (confirmTests) {
+                    return true;
+                    } else {
+                    return false;
+                    }
+                },
+            },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Please provide your email for support contact:',
+            validate: input => {
+                if  (input) {
+                    return true;
+                } else {
+                    console.log('Please enter your input !!');
+                    return false;
+                }
+            }
+        },
+    ]);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    promptUser();
+}
 
 // Function call to initialize app
 init();
