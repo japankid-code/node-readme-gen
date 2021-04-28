31d6fbfd9ecac5f35562e01a3f2ad6f2502ef3d5
japankid-code <japankid.jake@gmail.com>
2021-04-27 20:59:41 -0500

    add different licenses to readme generation, close #3

--- Develop/dist/README.md
+++ Develop/dist/README.md
@@ -1,65 +1,68 @@
+# node-readme-gen
 
-  # node-readme-gen
+cli app for creating readmes
 
-  node app generating readmes
-  
-  ## Table of Contents
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [License](#license)
-  - [Credits](#credits)
-  - [Tests](#tests)
-  - [Questions](#questions)
-  
-  ## Installation
-  
-  First of all, install [node](https://nodejs.org/en/).
-  Here are the list of commands used to get the application working:
-  
-  npm install inquirer
-  
-  ## Usage
-  
-  cli, produces markdown
-  
-  
-  ## License
-  
-  
-    MIT License
-    
-    Copyright (c) [year] [fullname]
-    
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-    
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
-  
-  
-  ## Contributing
-  
-  
-  
-  ## Tests
-  
-  
-  
-  ## Questions
-  
-  jpk.jake@gmails.com
-  
-  If your project has a lot of features, consider adding a "Features" section.
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Credits](#credits)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+First of all, install [node](https://nodejs.org/en/).
+Here are the list of commands used to get the application working:
+
+npm install inquirer
+
+## Usage
+
+respond to prompt, get readme :)
+Don't forget to add images! 
+
+![placeholder](https://via.placeholder.com/150)
+
+## License
+
+MIT License
+
+Copyright (c) 2021 Jake Rankin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+[MIT licensing info.](https://choosealicense.com/licenses/mit/)
+
+![MIT badge.](https://img.shields.io/badge/license-MIT-brightgreen)
+
+## Contributing
+
+nothing to [shout out](URLs)
+
+## Tests
+
+no testing procedures/coverage.
+
+## Questions
+
+japankid.jake@gmail.com
+
+If your project has a lot of features, consider adding a "Features" section.
--- Develop/index.js
+++ Develop/index.js
@@ -5,131 +5,144 @@ const { generateMarkdown } = require('./utils/generateMarkdown')
 
 const promptUser = () => {
     return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your application?',
-            validate: titleInput => {
-                if  (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the title !!');
-                    return false;
-                }
-            },
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Briefly describe the app:',
-            validate: descriptionInput => {
-                if  (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please describe the app!');
-                    return false;
-                }
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+        validate: titleInput => {
+            if  (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the title !!');
+                return false;
             }
         },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'What commands are needed to get the app working?',
-            validate: installationInput => {
-                if  (installationInput) {
-                    return true;
-                } else {
-                    console.log('Please enter commands used!!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'More detailed info about how to use:',
-            validate: usageInput => {
-                if  (usageInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your input !!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'Which license would you like to use?',
-            choices: ['MIT']
-        },
-        {
-            type: 'confirm',
-            name: 'confirmScreenies',
-            message: 'Would you like to add a link to a screenshot?',
-            default: false,
-        },
-            {
-                type: 'input',
-                name: 'screenshots',
-                message: 'URLs, separated by commas:',
-                when: ({ confirmScreenies }) => {
-                    if (confirmScreenies) {
-                    return true;
-                    } else {
-                    return false;
-                    }
-                },
-            },
-        {
-            type: 'confirm',
-            name: 'confirmContribs',
-            message: 'Are there any sources that need attributing?',
-            default: false,
-        },
-            {
-                type: 'input',
-                name: 'contributions',
-                message: 'Links, separated by commas:',
-                when: ({ confirmContribs }) => {
-                    if (confirmContribs) {
-                    return true;
-                    } else {
-                    return false;
-                    }
-                },
-            },
-        {
-            type: 'confirm',
-            name: 'confirmTests',
-            message: 'Were any tests made for the app??',
-            default: false,
-        },
-            {
-                type: 'input',
-                name: 'tests',
-                message: 'Describe testing process:',
-                when: ({ confirmTests }) => {
-                    if (confirmTests) {
-                    return true;
-                    } else {
-                    return false;
-                    }
-                },
-            },
-        {
-            type: 'input',
-            name: 'questions',
-            message: 'Please provide your email for support contact:',
-            validate: input => {
-                if  (input) {
-                    return true;
-                } else {
-                    console.log('Please enter your input !!');
-                    return false;
-                }
-            }
-        },
+      },
+      {
+          type: 'input',
+          name: 'title',
+          message: 'What is the title of your application?',
+          validate: titleInput => {
+              if  (titleInput) {
+                  return true;
+              } else {
+                  console.log('Please enter the title !!');
+                  return false;
+              }
+          },
+      },
+      {
+          type: 'input',
+          name: 'description',
+          message: 'Briefly describe the app:',
+          validate: descriptionInput => {
+              if  (descriptionInput) {
+                  return true;
+              } else {
+                  console.log('Please describe the app!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'installation',
+          message: 'What commands are needed to get the app working?',
+          validate: installationInput => {
+              if  (installationInput) {
+                  return true;
+              } else {
+                  console.log('Please enter commands used!!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'input',
+          name: 'usage',
+          message: 'More detailed info about how to use:',
+          validate: usageInput => {
+              if  (usageInput) {
+                  return true;
+              } else {
+                  console.log('Please enter your input !!');
+                  return false;
+              }
+          }
+      },
+      {
+          type: 'list',
+          name: 'license',
+          message: 'Which license would you like to use?',
+          choices: ['MIT', 'Unlicense', 'ISC']
+      },
+      {
+          type: 'confirm',
+          name: 'confirmScreenies',
+          message: 'Would you like to add a link to a screenshot?',
+          default: false,
+      },
+          {
+              type: 'input',
+              name: 'screenshots',
+              message: 'URLs, separated by commas:',
+              when: ({ confirmScreenies }) => {
+                  if (confirmScreenies) {
+                  return true;
+                  } else {
+                  return false;
+                  }
+              },
+          },
+      {
+          type: 'confirm',
+          name: 'confirmContribs',
+          message: 'Are there any sources that need attributing?',
+          default: false,
+      },
+          {
+              type: 'input',
+              name: 'contributions',
+              message: 'Links, separated by commas:',
+              when: ({ confirmContribs }) => {
+                  if (confirmContribs) {
+                  return true;
+                  } else {
+                  return false;
+                  }
+              },
+          },
+      {
+          type: 'confirm',
+          name: 'confirmTests',
+          message: 'Were any tests made for the app??',
+          default: false,
+      },
+          {
+              type: 'input',
+              name: 'tests',
+              message: 'Describe testing process:',
+              when: ({ confirmTests }) => {
+                  if (confirmTests) {
+                  return true;
+                  } else {
+                  return false;
+                  }
+              },
+          },
+      {
+          type: 'input',
+          name: 'questions',
+          message: 'Please provide your email for support contact:',
+          validate: input => {
+              if  (input) {
+                  return true;
+              } else {
+                  console.log('Please enter your input !!');
+                  return false;
+              }
+          }
+      },
     ]);
 }
 
